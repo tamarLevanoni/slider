4ac60a29ade5f096a838a4b699dcc64daed3bdd1
Tamar Levanoni <tamari250@gmail.com>
2021-01-12 12:23:13 +0200

    initial

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-// var slider = tns({
-//   container: ".my-slider",
-//   items: 3,
 
-//   responsive: {
-//     640: {
-//       edgePadding: 20,
-//       gutter: 20,
-//       items: 2,
-//     },
-//     700: {
-//       gutter: 30,
-//     },
-//     900: {
-//       items: 3,
-//     },
-//   },
-// });
 var slider2 = tns({
   container: "#customize",
   items: 3,
